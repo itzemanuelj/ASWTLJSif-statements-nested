b11e03efe78ddfe7c73fb6cbb7faa4df53ffa116
Emanuel Joness <itzemanuelj@gmail.com>
2022-09-01 14:42:27 -0400

    inital commit

---
+++ if statements nested.js
@@ -0,0 +1,30 @@
+// An alternative to using && to test multiple conditions in a single if statement is ______ ifs.
+
+nested
+
+// You communicate the nesting to JavaScript with the placement of what two characters? Don't type a space between them.
+
+{}
+
+// How do you make nested ifs readable? Answer with 1 lower-case verb.
+
+indent
+
+// This is the first line of an if statement.
+if (a !== b) {
+    if (a === b) {
+
+    }
+}
+
+// Enter the first 6 characters of the next line, which is a second-level if.
+
+// Create an if statement enclosing a nested if. If a equals b, then if c equals d, then f is assigned e.
+
+if (a === b) {
+    if (c === d){
+        e = f;
+    }
+}
+
+// Code the first line of an if statement that's followed by the first line of a nested if. If a equals b, then if c doesn't equal d....
